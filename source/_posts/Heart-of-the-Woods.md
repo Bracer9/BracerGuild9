2a14afdf1719d630299572abf74c0e03a2135a98
LI TAIXIN <bracer9@eruainonotobukkukonpyuta.local>
2024-12-01 20:35:52 +0900

    minor update

--- source/_posts/Heart-of-the-Woods.md
+++ source/_posts/Heart-of-the-Woods.md
@@ -10,7 +10,7 @@ categories:
 > 美国少女Vlogger二人组勇闯偏僻山村。
 > 一个关于成长，百合，勇气与传承的故事。
 
-游戏平台: [Steam](https://store.steampowered.com/app/844660/Heart_of_the_Woods/) | [官方网站](https://vnstudioelan.com/games/heart-of-the-woods/) | 制作团队: [Studio Elan](https://vnstudioelan.com/) 
+游戏平台： [Steam](https://store.steampowered.com/app/844660/Heart_of_the_Woods/) | [官方网站](https://vnstudioelan.com/games/heart-of-the-woods/) | 制作团队： [Studio Elan](https://vnstudioelan.com/) | 通关时长： 14小时
 <style>
   .custom-title {
     text-align: center;
--- themes/butterfly/source/js/protect.js
+++ themes/butterfly/source/js/protect.js
@@ -1,6 +1,23 @@
+/*
 window.onload = function() {
     var password = prompt("このページにアクセスするにはパスワードを入力してください:");
     if (password !== "ltx") {
       window.location.href = "https://google.com";  // パスワードが間違っている場合、リダイレクトする
+    }
+  };
+  */
+
+  window.onload = function() {
+    // セッションストレージに「passwordEntered」がない場合のみパスワードを要求
+    if (!sessionStorage.getItem("passwordEntered")) {
+      var password = prompt("パスワードを入力して:");
+  
+      if (password === "ltx") {
+        // 正しいパスワードが入力された場合
+        sessionStorage.setItem("passwordEntered", "true");  // セッションストレージに設定
+      } else {
+        // 間違ったパスワードが入力された場合
+        window.location.href = "https://google.com";  // 間違った場合、リダイレクト
+      }
     }
   };
